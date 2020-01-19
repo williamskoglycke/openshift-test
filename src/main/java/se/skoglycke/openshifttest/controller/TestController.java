fd5a1f4ad072c1d10a40d768b65e95743f118e58
William Skoglycke <william.skoglycke@tre.se>
2020-01-19 19:03:57 +0100

    openshift test

---
+++ src/main/java/se/skoglycke/openshifttest/controller/TestController.java
@@ -0,0 +1,15 @@
+package se.skoglycke.openshifttest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(path = "/echo/{echo}")
+    public String echo(@PathVariable final String echo) {
+        return echo;
+    }
+
+}
